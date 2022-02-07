e113353009afce2d8360b2917d59a3254a500a34
Bogdan748 <andriesbogdan00@gmail.com>
2022-02-07 21:19:16 +0200

    how to not repreat code when overloading

---
+++ oop-methods/oop-methods/ConsoleHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_methods
+{
+    public static class ConsoleHelper
+    {
+        public static void PrintMessage(string message)
+        {
+            PrintMessage(message, null, 1);
+        }
+
+        public static void PrintMessage(string label,string message)
+        {
+            PrintMessage(message, label, 1);
+        }
+
+        public static void PrintMessage(string message, uint count)
+        {
+            PrintMessage(message, null, count);
+            
+        }
+
+
+        public static void PrintMessage(string message, string label, uint count)
+        {
+
+            if (!string.IsNullOrEmpty(label))
+            {
+                Console.WriteLine($"{label}: ");
+            }
+
+            for (int i = 0; i < count; i++) 
+            {
+                Console.WriteLine(message);
+            }
+
+        }
+    }
+}
---
+++ oop-methods/oop-methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.PrintMessage("Text", "Hello World");
+        }
+    }
+}
